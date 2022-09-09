cd2317c0270a0caafb9feda234e155a3b44909a0
GoodOldFarmer <d.rodrigues@troisiemechambre.com>
2022-09-09 17:37:30 +0100

    app.js logic

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// console.log("hello")
+// 01_rps : javascript file for the Odin Project
+
+
+
+let computerSelection, playerSelection, cpuChoice
+
+// On définit un array avec les trois possibilités
+const arr = ["Rock", "Paper", "Scissors"];
+
+// Une fonction pour choisir une possibilité au hasard dans le arr
+function getComputerChoice(){
+
+    // On return un choix au hasard
+    return cpuChoice = arr[Math.floor(Math.random() * arr.length)];
+
+
+}
+
+
+getComputerChoice()
+console.log(cpuChoice)
